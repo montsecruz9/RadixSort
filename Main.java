0a46cdc914e908b87c9517e4ef70554e42207561
Montserrat Cruz <104812189+montsecruz9@users.noreply.github.com>
2024-01-15 13:18:23 -0600

    edit - adding sorting methods

--- Main.java
+++ Main.java
@@ -10,4 +10,26 @@ public class Main {
             System.out.println(radixArray[i]);
         }
     }
+
+    //radix sort method
+    public static void radixSort(int[] input, int radix, int width) {
+        for(int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix);
+        }
+    }
+
+    //radix single sort method
+    public static void radixSingleSort(int[] input, int position, int radix) {
+        int numItems = input.length;
+        int[] countArray = new int[radix];
+
+        for(int value: input) {
+            countArray[getDigit(position, value, radix)]++;
+        }
+    }
+
+    //getDigit method
+    public static void getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(10, position) % radix;
+    }
 }
