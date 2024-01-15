55a8e8dc10301ec07c0125dca2fda3a876de2e7c
Montserrat Cruz <104812189+montsecruz9@users.noreply.github.com>
2024-01-15 13:09:13 -0600

    uploading java file

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.example.radixsort;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] radixArray = {4725, 4586, 1330, 8792, 1594, 5729};
+
+        for (int i = 0; i < radixArray.length; i++) {
+            System.out.println(radixArray[i]);
+        }
+    }
+}
